137539d3fecd265821a06b057e0c4718f6940d71
Andrea Castellini <and1@live.it>
2017-05-12 17:30:33 +0200

    First commit

---
+++ __init__.py
@@ -0,0 +1,64 @@
+from .main import TNTVillage
+
+
+def autoload():
+    return TNTVillage()
+
+config = [{
+    'name': 'tntvillage',
+    'groups': [
+        {
+            'tab': 'searcher',
+            'list': 'torrent_providers',
+            'name': 'TNT Village',
+            'description': 'See <a href="http://www.tntvillage.scambioetico.org/">TNT Village</a>',
+            'icon':'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3gYbCDIHkb9iqwAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAJ7SURBVDjLfZLNS1RRGMZ/596Zq16/wmyslDFJLSExP3ZaobWpTQlmLqQPaBFECf4FbcKgXZTQIjRSiDLRFhLKGAhRotiANUYlpcJoUw2NM+Pc63jvaeNoDuaBszjvw/u8z/nxCiADUNn+rAEO0p0CG4jF7YrDJXpo0W8rIHRFjYqrdwsnVKeoSe6Mr5lcP/uIle43fOzqQsvMpP5hJ99eDPClvx+AJWvtuWLbrEgbkm/+7jKqik+TX1uLkq5zxefjwIkGMtxuLCmxgYhla4oQCCFAiM3pEpuGyksAKA4HMf8iP7xeALLcboRtIwABtuPnvLlgWTKgpagOV6GWA7DweTl4rK0lByBoGGawoCCaV1WVg5RkHyzmQyT8S9N1FuLxP46X95euAVkNrXvqXQe0J4oiqNzfOqUq2imAQGrqVOPAwARw0z8+zrvOTh5EovVEostAzAGEgfC+opSglGCuGjSfv3A88Z3I8vL3IyUlzQC+nh7mhocBFoHfAMo/4AWAru2i4lCdlijqul6qqupegK+DgwhVBTASupLUT115CwBerzcKUF1dXQ4wNzKCZZqbnJMNnM5UaaxGOFl5GYCxsbFpAJfLpQFM9PZaqqb93+DWxRFZU3qG3Gw3s7Oz5OXlvU5otpQM9/f7tlvVDYNgxC9unHsMgMfjedvU1BRMaB6Px1/kdE7taGDb1kasvledT8PhcG7iPTo62peVlmYmA98KcX0VLWkSis+HpJQhAMMwuNPR0R2X0rljglg0FvN9mmZmxmfebntWNzk5OQowNDQ0B7wPSTsasCwjYK1tgUhSrCqgAigDaG9vv6coSuO6ng0cBSpzVXVj8F8TPQGTxruMMQAAAABJRU5ErkJggg==',
+            'wizard': True,
+            'options': [
+                {
+                    'name': 'enabled',
+                    'type': 'enabler',
+                    'default': False,
+                },
+                {
+                    'name': 'username',
+                    'default': '',
+                    'description': 'Enter your site username.',
+                },
+                {
+                    'name': 'password',
+                    'default': '',
+                    'type': 'password',
+                    'description': 'Enter your site password',
+                },
+                {
+                    'name': 'seed_ratio',
+                    'label': 'Seed ratio',
+                    'type': 'float',
+                    'default': 1,
+                    'description': 'Will not be (re)moved until this seed ratio is met.',
+                },
+                {
+                    'name': 'seed_time',
+                    'label': 'Seed time',
+                    'type': 'int',
+                    'default': 40,
+                    'description': 'Will not be (re)moved until this seed time (in hours) is met.',
+                },
+                {
+                    'name': 'extra_score',
+                    'advanced': True,
+                    'label': 'Extra Score',
+                    'type': 'int',
+                    'default': 0,
+                    'description': 'Starting score for each release found via this provider.',
+                },
+		{
+		    'name': 'api_key',
+		    'label': 'API Key TMDB',
+		    'description': 'Your TMDB api key'
+       		}
+            ],
+        },
+    ],
+}]
---
+++ main.py
@@ -0,0 +1,121 @@
+from bs4 import BeautifulSoup
+from couchpotato.core.helpers.encoding import simplifyString, tryUrlencode
+from couchpotato.core.helpers.variable import tryInt
+from couchpotato import CPLog
+from couchpotato.core.media._base.providers.torrent.base import TorrentProvider
+from couchpotato.core.media.movie.providers.base import MovieProvider
+import datetime
+import traceback
+import re
+import requests
+import time
+import json
+log = CPLog(__name__)
+
+
+class TNTVillage(TorrentProvider, MovieProvider):
+	urls = {
+		'base_url': 'http://tntvillage.scambioetico.org',
+		'search': 'http://www.tntvillage.scambioetico.org/src/releaselist.php'
+	}
+	payload = None
+	desc = None
+	
+	http_time_between_calls = 1
+	
+	def _searchOnTitle(self, title, movie, quality, results):
+		if len(self.conf('api_key')) > 0:
+			try:
+				params = dict(
+					api_key=self.conf('api_key'),
+					language='it_IT'
+				)
+				url = "https://api.themoviedb.org/3/movie/%s" % movie['info']['tmdb_id']
+				resp = requests.get(url=url, params=params)
+				data = json.loads(resp.text)
+				title = data['title']
+			except:
+				log.error('Unable to find italian title')
+		row = []
+		payload = {'cat': 0, 'page': 1, 'srcrel': title}
+		data = requests.post(self.urls['search'], data=payload)
+		soup = BeautifulSoup(data.text)
+		row += soup.findAll('tr')
+		row.pop(0)
+		
+		if row and len(row) == 0:
+			log.info("No torrents found for %s on tntvillage", title)
+			return
+		if row:
+			try:
+				self.parseResults(results, row, movie['info']['year'], quality, title)
+			except:
+				log.error('Failed parsing TNTVillage')
+		else:
+			log.info('No search results found')
+	
+	def standardize_title(self, name, title, year, quality, desc):
+		s_y = re.findall(r'(\d{4})', name)
+		s_yo = "%d" % year
+		q_label = ""
+
+		for q in quality['alternative'] + [quality['label']] + [quality['identifier']]:
+			if str(q) in desc.lower():
+				q_label = desc
+
+		if s_yo in s_y:
+			ret = "%s (%d) - %s, %s" % (title, year, quality['identifier'], q_label)
+			log.info("NOME %s -> %s" % (name, ret))
+			return ret
+		return name
+
+	def calc_age(self, age_str):
+		import locale
+		locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+		tdelta = datetime.datetime.now() - datetime.datetime.strptime(age_str, " %b %d %Y")
+		return tdelta.days
+
+	def getTorrentInfo(self, link):
+		data = requests.get(link)
+		html = BeautifulSoup(data.content)
+		tds = html.findAll('td', text = re.compile('\d* mb', re.IGNORECASE))
+		size = 0
+		if len(tds) > 0:
+			size = tds[0].text
+		size = self.parseSize(size)
+
+		datas = html.findAll('span', attrs = {'class': 'postdetails'})
+		if len(datas) > 0:
+			datas = re.findall(r':(.*?),', datas[0].text)[0]
+		else:
+			datas = " Jan 1 1970"
+		age = self.calc_age(datas)
+		
+		titolo = html.findAll('td', id='sottotitolo')[1].text.strip('&nbsp;')
+		desc_match = re.compile(ur'\[(.*)\]')
+		s_res = re.search(desc_match, titolo)
+		if s_res:
+			g = s_res.group()
+		self.desc = g
+		return age, size
+
+	def parseResults(self, results, entries, year, quality, title):
+		new = {}
+		for result in entries:
+			tds = result.findAll('td')
+			
+			try:
+				new['age'], new['size'] = self.getTorrentInfo(tds[6].a['href'])
+				new['url'] = tds[0].a['href']
+				new['detail_url'] = tds[6].a['href']
+				new['id'] = tds[6].a['href'].split('showtopic=')[1]
+				new['name'] = self.standardize_title(tds[6].a.text, title, year, quality, self.desc)
+				new['seeders'] = tryInt(tds[4].text)
+				new['leechers'] = tryInt(tds[3].text)
+				new['score'] = self.conf('extra_score') + 20
+			except Exception as e:
+				log.info(e)
+				continue
+
+			results.append(new)
+			
